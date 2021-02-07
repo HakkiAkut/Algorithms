1e029f7f7ec54674a89a6ef8000d46c191c99205
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-02-07 22:27:28 +0300

    recursive solution

--- LongestIncreasingSubsequence/src/Main.java
+++ LongestIncreasingSubsequence/src/Main.java
@@ -7,12 +7,13 @@ public class Main {
         // 2, 4, 7, 12, 24
         // output must be 5
         System.out.println(subsequence(arr));
+        System.out.println(recursiveSubsequence(arr, 0, 0));
     }
 
     /**
      * Question:
      * Find longest increasing subsequence of given an integer array
-     *
+     * <p>
      * Dynamic Programming:
      * We hold max number of longest increasing subsequence from 0 to n in an array. ith element will check every smaller elements
      * before i than get max+1 as number of longest increasing subsequence from 0th to ith element.
@@ -40,5 +41,30 @@ public class Main {
             }
         }
         return max;
+    }
+
+    /**
+     * Recursive solution
+     *
+     * @param arr       is integer array that we will find longest increasing subsequence
+     * @param index     current index
+     * @param lastTaken last taken number from array
+     * @return number of longest increasing subsequence
+     */
+    public static int recursiveSubsequence(int[] arr, int index, int lastTaken) {
+        if (index == arr.length) {
+            return 0;
+        }
+        int current = 0;
+
+        if (lastTaken <= arr[index]) {
+            int result1 = recursiveSubsequence(arr, index + 1, arr[index]) + 1;
+            int result2 = recursiveSubsequence(arr, index + 1, lastTaken);
+            current = Math.max(result1, result2);
+        } else {
+            current = recursiveSubsequence(arr, index + 1, lastTaken);
+        }
+
+        return current;
     }
 }
